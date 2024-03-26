249c255b109acc5f4d0c964ef6c06750c554e6f2
Saverio <s.mosti6@studenti.unipi.it>
2024-03-26 17:25:45 +0100

    A commit

--- src/main/java/it/unipi/dii/generation/generationMainIndexes.java
+++ src/main/java/it/unipi/dii/generation/generationMainIndexes.java
@@ -13,26 +13,11 @@ public class generationMainIndexes {
         System.out.println("Start the generation of the indexes.");
 
         // Slips indexes.
-        createIndex(ms.mongoDB , "slips" , "confirmationDate" , -1);
-        createIndex(ms.mongoDB , "slips" , "username" , 1);
-        createIndex(ms.mongoDB , "slips" , "betList.matchID" , -1);
-
-        // Polls indexes.
-        createIndex(ms.mongoDB , "polls" , "activationDate" , -1);
-        createIndex(ms.mongoDB , "polls" , "pollType" , 1);
+        createIndex(ms.mongoDB , "slips" , "username" , 1); // Good index.
+        createIndex(ms.mongoDB , "slips" , "betList.matchID" , -1); // Good index.
 
         // Matches indexes.
-        createIndex(ms.mongoDB , "matches" , "matchDate" , -1);
-        createIndex(ms.mongoDB , "matches" , "status" , 1);
-
-        String[] indexFields = {"matchDate", "team_home" , "team_away"};
-        Integer[] indexOrder = { - 1, 1 , 1};
-        createCompoundIndex(ms.mongoDB , "matches" , indexFields, indexOrder);
-
-        // Customer indexes
-        indexFields = new String[]{"username", "name", "surname"};
-        indexOrder = new Integer[]{1, 1, 1};
-        createCompoundIndex(ms.mongoDB , "customers" , indexFields, indexOrder);
+        createIndex(ms.mongoDB , "matches" , "status" , 1); // Good Index
 
         System.out.println("Generation of the indexes ended.");
 
